848b1c17ea05465b9e11e52238bbef2db5d4a9a2
Mj Ansari <ansarimj14@gmail.com>
2023-09-24 22:21:38 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// console.log("Hello world!");
+// console.log (1+2);
+const a =10;
+const b =20;
+
+const sum = a+b;
+console.log(sum);
+// array
+const numbers = [10,20,30]
+console.log(numbers.length)
+const  person={
+    name: 'John',
+    age: 30
+}
+console.log (person.name)
