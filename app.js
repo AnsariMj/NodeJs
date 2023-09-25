47eae6e6d2c04cc2ff32b6c617bbfe0fe558b821
Mj Ansari <ansarimj14@gmail.com>
2023-09-25 21:59:22 +0545

    add

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 // console.log("Hello world!");
 // console.log (1+2);
-const a =10;
-const b =20;
+// const a =10;
+// const b =20;
 
-const sum = a+b;
-console.log(sum);
+// const sum = a+b;
+// console.log(sum);
 // array
-const numbers = [10,20,30]
-console.log(numbers.length)
-const  person={
-    name: 'John',
-    age: 30
-}
-console.log (person.name)
+// const numbers = [10,20,30]
+// console.log(numbers.length)
+// const  person={
+//     name: 'John',
+//     age: 30
+// }
+// console.log (person.name)
 //hello
 // Hello world
+const app = require("express")()
+
+app.get("/", (req, res) => {
+    // res.send("Hello world")
+    res.json({
+        greet: "Hello"
+    })
+})
+app.get("/contact", (req, res) => {
+    res.send("Hello ")
+})
+
+app.listen(3000, (req, res) => {
+    console.log("Nodejs is running on port 3000")
+})
